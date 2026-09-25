1fb7356337036fa09ee7be1a37a4b6235235fd32
Raphiyooo <raphael.strobl07@gmail.com>
2026-09-25 17:47:02 +0200

    lifo implementation

--- producer_consumer_problem.c
+++ producer_consumer_problem.c
@@ -6,24 +6,27 @@
 #include <semaphore.h>
 #include <fcntl.h>
 
-#define NUM_THREADS 2
+#define NUM_THREADS 4
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 sem_t* semaphore_full;
 sem_t* semaphore_empty;
 int buffer[10];
-int counter = 0;
+int in = 0;
+int out = 0;
 
 void* producer(void* args)
 {
   while (1)
   {
-    sem_wait(semaphore_empty);
+    sem_wait(semaphore_empty); // decrements empty by 1, initially it was 10, now 9
+    sleep(1);
     pthread_mutex_lock(&mutex);
     int x = rand() % 100;
-    buffer[counter++] = x;
+    buffer[in] = x;
+    in = (in + 1) % 10;
     pthread_mutex_unlock(&mutex);
-    sem_post(semaphore_full);
+    sem_post(semaphore_full); // increments full by 1, initially it was 0, now 1
   }
 }
 
@@ -31,12 +34,13 @@ void* consumer(void* args)
 {
   while (1)
   {
-    sem_wait(semaphore_full);
+    sem_wait(semaphore_full); // decrements full by 1, initially it was 1-9, now 0-8
+    sleep(1);
     pthread_mutex_lock(&mutex);
-    int y = buffer[counter - 1];
-    counter--;
+    int y = buffer[out];
+    out = (out + 1) % 10;
     pthread_mutex_unlock(&mutex);
-    sem_post(semaphore_empty);
+    sem_post(semaphore_empty); // increments empty by 1, initially it was 1-9, now 0-8
     printf("Got %d\n", y);
   }
 }
